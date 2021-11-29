ba36f42c81cc49e92cc1b6e3fc2cec923118181b
Khalid Salman-Yusuf <olaniyisal@gmail.com>
2021-11-29 09:26:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let colors = ["red", "pink", "orange", "brown", "purple", "lime"];
+
+let color = document.querySelector(".color");
+let btn = document.querySelector(".btn");
+
+
+btn.addEventListener("click", ()=>{
+    let randomNumber = getRandomNumber();
+    // console.log(randomNumber)
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+})
+
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length)
+}
+
+
